d722d642e38cfc839724c25051c51eff16bc4218
Milan Biranwar <milanbiranwar06@gmail.com>
2022-06-03 17:28:36 +0530

    Program for File IO

---
+++ Day27PracticeProblem/FileIO.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day27FileIO
+{
+    internal class FileIO
+    {
+        public static void FileExists()
+        {
+            string path = @"C:\Users\milan\source\repos\FileIODemo\FileIODemo\TextFile1.txt";
+            if (File.Exists(path))
+                Console.WriteLine("File exists");
+            else
+                Console.WriteLine("File doesn't exists");
+        }
+
+        public static void ReadAllLines()
+        {
+            String path = @"C:\Users\milan\source\repos\FileIODemo\FileIODemo\TextFile1.txt";
+            String[] lines;
+
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+
+        }
+
+        public static void ReadAllText()
+        {
+            String path = @"C:\Users\milan\source\repos\FileIODemo\FileIODemo\TextFile1.txt";
+            String lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+
+        }
+
+        public static void FileCopy()
+        {
+            String path = @"C:\Users\milan\source\repos\FileIODemo\FileIODemo\TextFile1.txt";
+            String copypath = @"C:\Users\milan\source\repos\FileIODemo\FileIODemo\Demo.txt";
+
+            File.Copy(path, copypath);
+
+        }
+
+        public static void DeleteFile()
+        {
+            String path = @"C:\Users\milan\source\repos\FileIODemo\FileIODemo\Demo.txt";
+
+            File.Delete(path);
+
+        }
+
+        public static void ReadFromStreamReader()
+        {
+            String path = @"C:\Users\milan\source\repos\FileIODemo\FileIODemo\TextFile1.txt";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                String s = String.Empty;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+
+        public static void WriteUsingStreamWriter()
+        {
+            String path = @"C:\Users\milan\source\repos\FileIODemo\FileIODemo\TextFile1.txt";
+
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hellow World - .Net is awesome1");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+    }
+}
--- Day27PracticeProblem/Program.cs
+++ Day27PracticeProblem/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Day27FileIO;
+
 Console.WriteLine("Hello, World!");
+
+FileIO.FileExists();
+FileIO.ReadAllLines();
+FileIO.ReadAllText();
+FileIO.FileCopy();
+FileIO.DeleteFile();
+FileIO.ReadFromStreamReader();
+FileIO.WriteUsingStreamWriter();
